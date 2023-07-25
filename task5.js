6e20281054ea0352a8b22212c5537a0741b14ea3
sahik7 <sahikahsan7@gmail.com>
2023-07-25 16:21:44 +0600

    task 5 solved

---
+++ task5.js
@@ -0,0 +1,15 @@
+function calculator(num1, num2, operator) {
+    if (operator === '+') {
+        return num1 + num2;
+    } else if (operator === '-') {
+        return num1 - num2;
+    } else if (operator === '*') {
+        return num1 * num2;
+    } else if (operator === '/') {
+        return num1 / num2;
+    }
+}
+
+
+const result = calculator(10, 2, "/");
+console.log(result); 
